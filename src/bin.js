97ed4ac11ccf2ffe479b94ba2359ef8bb077f7f5
Henrique Dias <hacdias@gmail.com>
2018-10-30 00:22:39 +0000

    feat: async await
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/bin.js
+++ src/bin.js
@@ -4,10 +4,9 @@
 
 const PeerId = require('./index.js')
 
-PeerId.create((err, id) => {
-  if (err) {
-    throw err
-  }
+async function main () {
+  const id = await PeerId.create()
+  console.log(JSON.stringify(id.toJSON(), null, 1))
+}
 
-  console.log(JSON.stringify(id.toJSON(), null, 2))
-})
+main()
